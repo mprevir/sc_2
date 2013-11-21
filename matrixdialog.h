a57c64dd1b9bf64a8b28945e3975a96181d421b1
Mykola Previr <m.previr@samsung.com>
2013-11-21 18:10:40 +0200

    Reorganisation

---
+++ matrixdialog.h
@@ -0,0 +1,30 @@
+#ifndef MATRIXDIALOG_H
+#define MATRIXDIALOG_H
+
+#include <QDialog>
+#include <vector>
+#include <stdint.h>
+
+namespace Ui {
+class MatrixDialog;
+}
+
+class MatrixDialog : public QDialog
+{
+    Q_OBJECT
+    
+public:
+    explicit MatrixDialog(QWidget *parent = 0);
+    void setMatrix(const std::vector< std::vector<int32_t> >& iMatrix);
+    void showMatrix();
+    ~MatrixDialog();
+    
+private slots:
+    void on_closeButton_clicked();
+
+private:
+    Ui::MatrixDialog *ui;
+    std::vector< std::vector<int32_t> > matrix;
+};
+
+#endif // MATRIXDIALOG_H
